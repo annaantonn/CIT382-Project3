b00ef6d09af1fc835911fb402c85fb67a5089a7f
annaantonn <105316891+annaantonn@users.noreply.github.com>
2023-02-27 15:30:05 -0800

    Create JupiterMoonData.js

---
+++ JupiterMoonData.js
@@ -0,0 +1,26 @@
+export const JupiterMoonData = [
+  {
+    name: 'IO',
+    diameterKM: 3642,
+    image: 'IO.jpg',
+    link: 'https://en.wikipedia.org/wiki/Io_(moon)'
+  },
+  {
+    name: 'Eupopa',
+    diameterKM: 3121,
+    image: 'Europa.jpg',
+    link: 'https://en.wikipedia.org/wiki/Europa_(moon)'
+  },
+  {
+    name: 'Ganymede',
+    diameterKM: 5268,
+    image: 'Ganymede.jpg',
+    link: 'https://en.wikipedia.org/wiki/Ganymede_(moon)'
+  },
+  {
+    name: 'Callisto',
+    diameterKM: 4820,
+    image: 'Callisto.jpg',
+    link: 'https://en.wikipedia.org/wiki/Callisto_(moon)'
+  }
+];
